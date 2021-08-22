9d2a176e18f431976f4bf3a7d78bef7bae97c3b5
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-23 00:20:38 +0900

    2.3 const and let

---
+++ momentum/app.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = 3;
+
+console. log(a+b);
+console. log(a*b);
+console. log(a/b);
+
+let myName = "nicolas";
+
+console. log(myName);
+
+myName = "Junje";
+
+console. log("your new name is " + myName);
