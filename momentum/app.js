148d25af7119013f1d7cc96ab6ef572c4a4a282a
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-23 18:03:45 +0900

    2.5 array

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,24 @@
-const amIFat = null;
-/*없다는 것을 확실히 할때 쓰인다. 자연적으로는 절대 나오지 않음*/
-let something; 
-/*undefined*/
-console. log(something);
+const mon ="mon";
+const tue ="tue";
+const wed ="wed";
+const thu ="thu";
+const fri ="fri";
+const sat ="sat";
+
+const dayOfWeek = [mon, tue, wed, thu, fri, sat]
+/*""안에 넣어줘도 된다.*/
+
+console.log(dayOfWeek);
+/*컴퓨터는 0부터 숫자를 센다.*/
+
+dayOfWeek.push("sun");
+
+console.log(dayOfWeek);
+//array는 하나의 항목안에 리스트를 만드는 것.
+
+const apple ="apple";
+//이런식으로는 너무 길어서 리스트를 만들 수 없다.
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab"); //kimbab을 array 안으로 추가
+console.log(toBuy[2]);
