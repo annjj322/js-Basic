98a528ddbd4a7ed5683c689200c5bd614c5faca3
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-24 16:33:38 +0900

    2.7 Functions

--- momentum/app.js
+++ momentum/app.js
@@ -1,15 +1,18 @@
-const character = {
-    handsome: true,
-    heigh: 180,
-    age: 21,
-    name: "june",
-}
+//console.log("Hello my name is Nico");
+//console.log("Hello my name is Dal");
+// 너무 복잡해서 생긴 function
+//뒤에 이름만 바꾸면 됨.
 
-console.log(character);
-character.heigh=175;
-console.log(character);
-character.girlFriend=false;
-console.log(character);
-character.age=character.age + 5;
-console.log(character);
-console.log(character["name"]);
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson +" and I'm " + age);
+}
+/*
+sayHello();
+sayHello();
+sayHello();
+실행시키는 방법
+*/ 
+
+sayHello("june", 15);
+sayHello("dal", 24);
+sayHello("lynn", 21);
