9acd101b072c90ed5b326aea86c1d59b24222b64
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-24 19:19:44 +0900

    2.9 Function

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,9 @@
-function plus(a, b){
-    console.log(a+ b);
-}
-//NaN = Not a Number
-plus(8, 60);
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName){
+    console.log("hello! " + otherPersonsName + " nice to meet you");
+    }
+};
 
-//plus(a, b)와 같은 형식을 넣어주고 형식에 맞게 값을 대입하면
-//{}내부의 매뉴얼이 실행되도록 하는 기능. Function
+console.log(player.name);
+player.sayHello("june");
