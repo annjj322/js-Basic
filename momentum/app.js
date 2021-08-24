82ce9240e5e8c63e45ac9c35bae649fc2d2253b1
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-24 20:29:49 +0900

    2.11 Returns

--- momentum/app.js
+++ momentum/app.js
@@ -1,19 +1,44 @@
+
 const calculator = {
     add:function (a, b) {
-        console.log(a + b);
+        return a + b;
     },
     minus:function (a, b) {
-        console.log(a - b);
+        return a - b;
     },
     div:function (a, b) {
-        console.log(a / b);
+        return a / b;
     },
     powerOf:function (a, b) {
-        console.log(a ** b);
+        return a ** b;
     },
 };
+/*
+console은 외부로 표현되지 않는다.
+alert 또한 경고하고 없어진다.
+*/
+const plusResult = calculator.add(2,3);
+const minusResult = calculator.minus(plusResult,10);
+const divResult = calculator.div(10,minusResult);
+const powerOfResult = calculator.powerOf(minusResult,divResult);
+/*
+반환값을 function에서 얻는다.
+위 값들은 상호 의존적 console로는 할 수 없었던 것.
+*/
 
-calculator.add(5,2);
-calculator.minus(5,2);
-calculator.div(5,2);
-calculator.powerOf(5,2);
+const age =96;
+function calculateKrAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
+
+// age-> 96
+//const krAge의 age -> 96
+//ageOfForeigner -> 96
+//return 되는 값 -> 98
+//console.log(krAge); ==> 98
+
+//return을 하는 이유는 콘솔이 아닌 화면에 결과값을 보여줘야되기 때문.
