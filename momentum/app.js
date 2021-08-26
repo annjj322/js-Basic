5aaa0b6f7a80afc3d5b0a28479fe5bed4592ee1d
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-26 18:35:12 +0900

    3.4 MouseLeave, MouseEnter

--- momentum/app.js
+++ momentum/app.js
@@ -4,4 +4,15 @@ function handleTitleClick() {
     title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+    title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(params) {
+    title.innerText = "Mouse is gone";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
