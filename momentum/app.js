f5403b827dfb20cf27110947a805e48c443e7991
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-26 18:47:20 +0900

    3.5 More Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,18 +1,41 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-function handleTitleClick() {
-    title.style.color = "blue";
+function handleh1Click() {
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter() {
-    title.innerText = "Mouse is here";
+    h1.innerText = "Mouse is here";
 }
 
-function handleMouseLeave(params) {
-    title.innerText = "Mouse is gone";
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
 
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("Now Its Okay");
+}
+
+h1.addEventListener("click", handleh1Click);
+//= h1.onclick = handleh1Click;
+//addEventLIstener를 사용하는 이유는 remove--를 사용하기 위함
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
