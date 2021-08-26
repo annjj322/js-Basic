a098bb1de5bb43a6a674ae7922e38db3806f7172
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-26 17:20:21 +0900

    3.4 Click Event

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,7 @@
 const title = document.querySelector(".hello h1");
-//많은 양의 데이터를 가져올때 사용하는 함수 = getElementsByClassName
-//element 하나만 선택해서 가지고 올때 사용하는 함수 = getElementsByTagName
-//querySelector는 css스타일로 데이터를 가져올 수 있다.
-//ㄴ> h1이 많다면 첫번째 것만 나온다. (first element)
-//ㄴ> querySelectorAll을 해야 조건에 맞는 모든 element를 불러온다.
 
-console.log (title);
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
