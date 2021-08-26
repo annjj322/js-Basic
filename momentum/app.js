35ec53e5b44fcb911cac250d642972883a04b7a9
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-08-26 16:59:31 +0900

    3.2 Searching For Elements

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,8 @@
-document.getElementById("title");
-//string을 호출하는 함수
+const title = document.querySelector(".hello h1");
+//많은 양의 데이터를 가져올때 사용하는 함수 = getElementsByClassName
+//element 하나만 선택해서 가지고 올때 사용하는 함수 = getElementsByTagName
+//querySelector는 css스타일로 데이터를 가져올 수 있다.
+//ㄴ> h1이 많다면 첫번째 것만 나온다. (first element)
+//ㄴ> querySelectorAll을 해야 조건에 맞는 모든 element를 불러온다.
 
-console.dir(title);
-
-title.innerText="Got you";
-//Grab me 부분을 JS로 변환
-
-console.log(title.id);
-console.log(title.className);
-//console에 title id와 className 을 불러온다.
+console.log (title);
